6672a54aede58d829959fb494950d5645fda9de4
Drew <noone@somewhere.com>
2020-02-23 13:43:12 -0500

    find common dir working

---
+++ c#/People/People/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.Model
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        public string[] Names { get; set; }
+
+        public string Role { get; set; }
+
+        public int SupervisorId { get; set; }
+    }
+}
